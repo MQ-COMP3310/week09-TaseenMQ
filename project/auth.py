86ab954b7cdf48102ea2fa6c506c0b9556f8995e
TaseenMQ <ahmedtaseen.muntaha@students.mq.edu.au>
2025-05-08 10:57:57 +1000

    auth.py

--- project/auth.py
+++ project/auth.py
@@ -41,7 +41,7 @@ def signup_post():
 
     signup_query=text('select * from user where email = :email')
     
-    user = db.session.execute(signup_query, {"user_email": email}).all() # check if a user with the supplied email already exists
+    user = db.session.execute(signup_query, {"email": email}).all() # check if a user with the supplied email already exists
     if len(user) > 0: # if a user is found, we want to redirect back to signup page so user can try again
         flash('Email address already exists')  # 'flash' function stores a message accessible in the template code.
         app.logger.debug("User email already exists")
